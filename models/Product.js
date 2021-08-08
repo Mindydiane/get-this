ae4f2d19399bc9f9cdfc44db26c3750728d19c51
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-08 03:36:13 -0500

    trying out codes for mysql db server

---
+++ models/Product.js
@@ -0,0 +1,57 @@
+// import important parts of sequelize library
+const { Model, DataTypes, DECIMAL } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Doesn't allow null values
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true
+    },
+    product_name: {
+      // String
+      type: DataTypes.STRING,
+      //Doesn't allow null values
+      allowNull: false
+    },
+    price: {
+      // Decimal
+      type: options.decimals,
+      //Doesn't allow null values
+      allowNull: false,
+      //Validates that the value is a decimal
+      validate: DECIMAL
+    },
+    stock: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Doesn't allow null values
+      allowNull: false,
+      // Set a default value of 10
+ 
+      //Validates that the value is numeric
+      
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
