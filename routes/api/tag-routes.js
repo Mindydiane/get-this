41b6d28d5b79289e341ee98ca1414f69bcd2bcf1
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-08 18:34:43 -0500

    find a single tag on tag routes added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,19 +10,34 @@ router.get("/", (req, res) => {
     include: [
       {
         model: Product,
-      }
-    ]
+      },
+    ],
   })
-  .then(dbTagData => res.json(dbTagData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    // be sure to include its associated Product data
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  })
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
