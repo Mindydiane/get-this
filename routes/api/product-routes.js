37abb4464d5afb923ac904ec332c0a0be2ea601b
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-11 19:55:57 -0500

    cleaned up code for product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,17 +2,13 @@ const router = require("express").Router();
 const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
-
 // get all products
 router.get("/", (req, res) => {
   // find all products
-  Product.findAll({
-    // attributes: ["id", "product_name", "price", "stock", "category_id"],
-    
+  Product.findAll({   
     // be sure to include its associated Category and Tag data
     include: [
-      Category,
-      
+      Category,     
       {
         model: Tag,
         through: ProductTag
@@ -41,22 +37,12 @@ router.get("/:id", (req, res) => {
       {
         model: Tag,
         through: ProductTag
-        // attributes: ["id", "tag_name"]
       }
     ]
   })
-  .then (dbProductData => {
-    // find way to get 1st item in object of dbProductData
-    
-    // if(!dbProductData) {
-    //   res.status(404).json({message: "no product found with this id"});
-    //   return;
-    // }
+  .then (dbProductData => { 
     console.log(dbProductData);
-    res.json(dbProductData);
-    // console.log(Object.keys(dbProductData)[0])
-    // return res.json(Object.keys(dbProductData)[0])
-
+    res.json(dbProductData);   
   })
   .catch(err => {
     console.log(err);
@@ -66,14 +52,6 @@ router.get("/:id", (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
-  /* create a new post
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
